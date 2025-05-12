b4e1eb8f6baf261bf0f9a64c7e882715eb7365ed
mianinaam <inaamking777@gmail.com>
2025-05-12 09:38:53 +0500

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000; i++) {
+            if (isPrime(i) && isPrime(i + 2)) {
+                System.out.println("(" + i + ", " + (i + 2) + ")");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
